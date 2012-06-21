3f47fd5e183275aac15f735e19ac7c7ffa4f343a
Kaique da Silva <kaique.developer@gmail.com>
2012-06-21 17:00:17 -0300

    Remove logic in progress

--- action/crud.js
+++ action/crud.js
@@ -33,7 +33,7 @@ var CRUD = (function () {
 
 		list: function ( settings ) {
 			// List data in memory
-			var output = [];
+			var output = false;
 
 			settings = settings || {};
 
@@ -73,16 +73,61 @@ var CRUD = (function () {
 			return output;
 		},
 
-		insert: function () {
+		insert: function ( settings ) {
 			// Create a new item
+			var output = false;
+
+			settings = settings || {};
+
+			if ( settings && 'collection' in settings ) {
+				underscore.map( memory, function ( item ) {
+					var keys = underscore.keys( item );
+
+					for ( var key in keys ) {
+						if ( settings['collection'] === keys[ key ] ) {
+								var collection = item[ settings['collection'] ];
+
+								if ( 'data' in settings && settings['data'] ) {
+									collection.push( settings['data'] );
+									output = true;
+								}
+						}
+					}
+				});
+			}
+
+			return output;
 		},
 
 		edit: function () {
 			// Edit a item
 		},
 
-		remove: function () {
-			// Remove item
+		remove: function ( settings ) {
+			// List data in memory
+			var output = false;
+
+			settings = settings || {};
+
+			if ( settings ) {
+				var result = underscore.map( memory, function ( collections ) {
+					if ( underscore.has( collections, settings['collection'] ) ) {
+						return underscore.map( collections, function ( collection ) {
+							for ( var filter in settings['filter'] ) {
+								if ( underscore.has( collection, filter ) && collection[ filter ] === settings['filter'][ filter ] ) {
+									return collections;
+								}
+							}
+						});
+					}
+				});
+
+				if ( !underscore.isEmpty( underscore.compact( result ) ) ) {
+					output = result;
+				}
+			}
+
+			return output;		
 		},
 	};
 
