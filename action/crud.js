a9e7e8d680a80279eeeca97f0ed8e8b8a9df80f1
Kaique da Silva <kaique.developer@gmail.com>
2012-06-21 13:45:18 -0300

    List memory ok, test in progress

--- action/crud.js
+++ action/crud.js
@@ -2,6 +2,7 @@ var CRUD = (function () {
 	// Book action to basic CRUD
 	var _ = function () {};
 
+	var underscore = require('underscore');
 	var memory = null;
 
 	_.prototype = {
@@ -36,8 +37,37 @@ var CRUD = (function () {
 
 			settings = settings || {};
 
-			if ( settings ) {
-				
+			if ( settings && 'collection' in settings ) {
+				underscore.map( memory, function ( item ) {
+					var keys = underscore.keys( item );
+
+					for ( var key in keys ) {
+						if ( settings['collection'] === keys[ key ] ) {
+							if ( 'filter' in settings ) {
+								var collection = item[ settings['collection'] ],
+									result = {};
+
+								for ( var field in collection ) {
+									var line = collection[ field ];
+
+									for ( var fieldName in line ) {
+										for ( var filterID in settings['filter'] ) {
+											var filterName = settings['filter'][ filterID ]
+											
+											if ( fieldName === filterName ) {
+												result[ fieldName ] = line[ fieldName ];
+											}
+										}
+									}
+								}
+
+								output = result;
+							} else {
+								output = item[ settings['collection'] ];
+							}
+						}
+					}
+				});
 			}
 
 			return output;
